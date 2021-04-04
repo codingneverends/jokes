b162eb97eb8c81f149ebf59d86ec5752569d036e
codingneverends <gaganlal7171@gmail.com>
2021-04-04 11:52:25 +0530

    g_c

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useEffect,useState} from 'react'
 function App() {
+  useEffect(()=>{
+    console.log("Hello");
+    GetJokes();
+  },[]);
+  async function GetJokes(){
+    const fecthdata=await fetch("https://v2.jokeapi.dev/joke/Programming");
+    const data=await fecthdata.json();
+    console.log(data);
+    if(data.joke){
+      console.log(data.joke);
+      setjoke(data.joke);
+      setjoke_(" ");
+    }
+    else{
+      setjoke(data.setup);
+      setjoke_(data.delivery);
+    }
+  }
+  const [joke,setjoke]=useState();
+  const [joke_,setjoke_]=useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{maxWidth:"500px",display:"block"}}>
+      <h1 className="head1">Radom Programming Jokes</h1>
+      <br/>
+      <h3>{joke}</h3>
+      <br/>
+      <h3>{joke_}</h3>
+      <br/>
+      <br/>
+      <center>
+      <button onClick={GetJokes}>Random Jokes</button>
+      </center>
     </div>
   );
 }
